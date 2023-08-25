a8ea005708c34b8018b3050274763ce4e2c8f44b
Sahil Bathla <99262172+besahilll@users.noreply.github.com>
2023-08-25 23:55:51 +0530

    Create Java_Strings.java
    This is the code to understand the concept of strings using some of its functions.

---
+++ Java/Java Basics/Java_Strings.java
@@ -0,0 +1,34 @@
+/* 
+The elements of a String are called characters. The number of characters in a String is called the length, and it can be retrieved with the String.length() method.
+
+Given two strings of lowercase English letters,  and , perform the following operations:
+
+Sum the lengths of  and .
+Determine if  is lexicographically larger than  (i.e.: does  come before  in the dictionary?).
+Capitalize the first letter in  and  and print them on a single line, separated by a space.
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length() + B.length());
+        if(A.compareTo(B) >0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+        System.out.println(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
+        
+    }
+}
+
+
